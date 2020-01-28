dfe06d79299165a500b6ecba5d4cfeca7ea6874f
Caeman Toombs <caeman.toombs@bitsighttech.com>
2020-01-28 01:15:30 -0500

    Well thaaaaaaat's okay.

--- git/git.sh
+++ git/git.sh
@@ -1,18 +1,79 @@
 include $rc/git-subrepo/.rc
 
 # TODO convert git-extensions to git aliases
-ln -sf $rc/git/gitconfig ~/.gitconfig
 
-gitdir=~/my
-gitremote=https://github.com/toombs-caeman
-gitemail='toombs.caeman@gmail.com'
-# make cloning a breeze
+# link the gitconfig out to home
+[[ -f ~/.gitconfig ]] || ln -s $rc/git/gitconfig ~/.gitconfig
+
+export gtree_dir=~/my/
+gtree_user=toombs-caeman
 my() {
-    [[ ! -d $gitdir/$1 ]] && [[ ! -z "$1" ]] && git -C $gitdir clone $gitremote/$1.git
-    cd $gitdir/$1
+    # collect cloned git repositories into a single directory
+    # using a 'user/repo' directory structure
+    # configured with gtree_{dir,protocol,remote,user}
+    # will fail to clone if gtree_user is not set and user field is not provided
+
+    # set defaults
+    local protocol remote user repo
+    protocol=${gtree_protocol:-https}
+    remote=${gtree_remote:-github.com}
+
+    # figure out the structure of the input
+    # $1 could be 'repo', 'user/repo','site/user/repo', or 'protocol//site/user/repo'
+    # don't set user or repo, if only the repo was set, then default to * for lookup
+    # 	but to $gtree_user for cloning using ${:-}
+    readarray -td/ a <<<"$1"
+    case ${#a[@]} in
+        4) protocol=${a[0]}; 	a=("${a[@]:1}");&
+        3) remote=${a[0]}; 	a=("${a[@]:1}");&
+        2) user=${a[0]}; 	a=("${a[@]:1}");&
+        1) repo=${a[0]}; 	a=("${a[@]:1}");;
+    esac
+
+    #echo protocol: $protocol
+    #echo remote: $remote
+    #echo user: $user
+    #echo repo: $repo
+
+    # if the directory exists, go there and exit
+    d=$(cd $gtree_dir; for r in ${user:-*}/${repo:-*}; do echo "$r"; done | fzf -1)
+    # if we selected something, try to go there
+    # if both user and repo are set, but the repo hasn't been cloned yet
+    # 	fzf will auto-select the one option, cd will fail, and we'll continue on below
+    [[ ! -z "$d" ]] && cd $gtree_dir/$d 2>/dev/null && return
+
+
+    # the directory doesn't exist, so try to clone it and then go there
+    
+    # construct full path
+    # normalize weirdness related to ssh naming
+    remote=${remote#git@} # remove possible remote user git
+    repo=${repo%.git} # remove possible .git on repo name
+    local full_path
+    case $protocol in
+        http)  ;& # disallow http by dropping through to https
+        https)
+            full_path=https://$remote/$user/$repo
+            ;;
+        ssh)
+            full_path=ssh://git@$remote:$user/$repo.git
+            ;;
+        *)
+            echo unrecognized protocol $protocol
+            return 1
+            ;;
+    esac
+
+    echo remote: $full_path
+    # ensure user directory is there
+    mkdir -p $gtree_dir/${user:-$gtree_user}
+    # clone and go
+
+    git -C $gtree_dir/${user:-$gtree_user} clone $full_path \
+        && cd $gtree_dir/$user/$repo
 }
 _my_complete() {
-    COMPREPLY=($(compgen -W "$(ls $gitdir)" "${COMP_WORDS[1]}"))
+    COMPREPLY=($(compgen -W "$(cd $gtree_dir;for r in */*; do echo $r;echo ${r#*/}; done)" "${COMP_WORDS[1]}"))
 }
 complete -F _my_complete my
 
--- rc.sh
+++ rc.sh
@@ -13,30 +13,53 @@ SHL_CONTEXT_NEXT=
 export rc=$(realpath $(dirname $BASH_SOURCE))
 
 ctx() {
+    # start a subshell with an additional rc that *almost* duplicates the default context completely
+    # the only things not passed are unexported variables
+    # this isolates changes made in context from the default shell, yet alllows the common configuration
+    # to apply generally
+
     # with no arguments or if we're already in the right context, print the context and return
     if [[ $# -eq 0 ]] || [[ "$1" == "$SHL_CONTEXT" ]]; then
         echo $SHL_CONTEXT
         return 0
     fi
 
-    # if we're in default, start the context subshell
-    # capture &5 as the context to enter next
+    # if we're in default, start the context subshell.
+    # capture the exit code to indicate if we should enter another context
     if [[ "$SHL_CONTEXT" == "default" ]]; then
-        SHL_CONTEXT=$1 bash -i
+        # duplicate shell options, functions, completions, and aliases explicitly
+        # then include the context specific init.sh
+        # the enironment is already passed by default and .bashrc is not run
+        SHL_CONTEXT=$1 bash --rcfile <(
+        	shopt -p
+        	declare -f
+        	complete
+        	alias
+        	cat $rc/contexts/$1/init.sh 2>/dev/null
+        	) -i
+
+        # capture the exit code from bash.
+        # We'll arbitrarily use 60 as an exit code denoting that we should go to context[0] next
+        # One quirk of this method is that you can't enter arbitrary contexts from anywhere except default.
+        # 	if you want to start a context with no extra configuration (and no init.sh)
+        # 	you have to do that from default
+    	# the new shell isn't started here because of **scary recursion**.
+    	# 	just set a flag so it's started by the prompt command in the default shell
         err=$(($? - 60))
         readarray -t a <<<$(ls $rc/contexts)
+        # negative values will index from the back, but indexing off the end will expand to a null string
+        # this is actually desired, since it probably means the exit code wasn't set by ctx()
         [[ $err -ge 0 ]] && SHL_CONTEXT_NEXT=${a[$err]} || SHL_CONTEXT_NEXT=
         return 0
     fi
     # since we're not in the right context
     # use the return code to indicate which context comes next
-    # the new shell isn't started here because of **scary recursion**
-    # it's started by the prompt command in the default shell
     readarray -t a <<<$(ls $rc/contexts)
     for index in "${!a[@]}"; do
         [[ "${a[index]}" == "$1" ]] && silent exit $(($index + 60))
     done
     echo context $1 not found
+    return 1
 }
 _ctx_complete() {
     COMPREPLY=($(compgen -W "$(ls $rc/contexts)" "${COMP_WORDS[1]}"))
@@ -58,7 +81,7 @@ include() {
     # include files, or entire directories
     [[ -f "$1$2" ]] && . $1$2
     if [[ -d "$1" ]]; then
-    	# by deult include .sh files only
+    	# by default include .sh files only
 	for file in $1/*${2:-.sh}; do
             . $file
 	done
@@ -82,5 +105,7 @@ include $rc/git
 # then write the history
 export PROMPT_COMMAND="err=\$?; [[ ! -z \"\$SHL_CONTEXT_NEXT\" ]] && ctx \$SHL_CONTEXT_NEXT; history -w"
 
-# include the context if we're doing that
+# include the context rc if we're not in default
+# this is not generally run since this file isn't sourced when starting the context
+# but if we source it manually, also run the context init
 [[ "$SHL_CONTEXT" != "default" ]] && . $rc/contexts/$SHL_CONTEXT/init.sh || true
